cc4eab4e50bd8eff45450c15986731768ab0308c
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-08-12 16:19:16 +0200

    Create steadystate_is_reached.m
    check whether steady state is reached

---
+++ IbM_V2/steadystate_is_reached.m
@@ -0,0 +1,13 @@
+function isReached = steadystate_is_reached(conc0, conc)
+    % Check whether a steady state is reached during the diffusion
+    % 
+    % conc0: last concentration values
+    % conc: latest concentration values
+    %
+    % -> isReached: boolean signifying whether steady state is reached
+
+    
+    
+    
+    
+end
