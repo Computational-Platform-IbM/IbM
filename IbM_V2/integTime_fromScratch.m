16cf28656f37afaacd14ed4ddf59c2f24f250fc4
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-08-12 10:46:00 +0200

    implement set_concentrations

--- IbM_V2/integTime_fromScratch.m
+++ IbM_V2/integTime_fromScratch.m
@@ -10,10 +10,10 @@ function integTime_fromScratch(grid, bac, conc, directory, constants, init_param
     diffusion_region = determine_diffusion_region(grid2bac, grid2nBacs, bac, grid, constants); 
     
     % initialise concentrations
-    conc = set_starting_concentrations(conc, constants.init_concs, diffusion_region);
+    conc = set_concentrations(conc, init_concs, diffusion_region);
     
     % set bulk layer concentrations
-    conc = set_bulk_concentrations(conc, bulk_concs, diffusion_region);
+    conc = set_concentrations(conc, bulk_concs, ~diffusion_region);
 
     
     % set time_indices
