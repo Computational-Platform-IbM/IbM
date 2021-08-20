e426d0322e9f2116e6a8e46fb33c652e097ce0b2
soundslikealloy <eloi.mrp@gmail.com>
2021-08-20 06:03:03 +0100

    kDist constant missing in quadtree

--- IbM_V2/lib/bacteria/bacteria_shove.m
+++ IbM_V2/lib/bacteria/bacteria_shove.m
@@ -5,8 +5,8 @@ function bac = bacteria_shove(bac, constants)
     % constants: struct containing all simulation constants
     %
     % -> bac: see above
-
-    r = quadtree.pushing2D(length(bac.x), bac.x, bac.y, bac.radius, 0.1, constants.bac_max_radius * 2);
+    
+    r = quadtree.pushing2D(length(bac.x), bac.x, bac.y, bac.radius, 0.1, constants.bac_max_radius * 2, constants.kDist);
     bac.x = r.bac_x;
     bac.y = r.bac_y;
 end
