046a70c7d5ef18c6f3bdc0e0a6f36d648791d449
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-08-23 14:02:40 +0200

    add profiling and fix bulk_conc

--- IbM_V2/lib/integTime.m
+++ IbM_V2/lib/integTime.m
@@ -1,7 +1,7 @@
-function integTime(grid, bac, conc, directory, constants, init_params)
+function profiling = integTime(grid, bac, conc, directory, constants, init_params)
     %% initialisation
     % calculate boundary conditions
-    bulk_concs = calculate_bulk_concentrations(constants, init_params.init_bulk_conc, init_params.invHRT, 0, constants.dT);
+    [bulk_concs, invHRT] = calculate_bulk_concentrations(constants, init_params.init_bulk_conc, init_params.invHRT, 0, constants.dT);
     
     % make bacterial-grid matrix
     [grid2bac, grid2nBacs] = determine_where_bacteria_in_grid(grid, bac);
@@ -26,23 +26,35 @@ function integTime(grid, bac, conc, directory, constants, init_params)
     Time.divide = constants.dT_divide;
     Time.save = constants.dT_save;
    
-       
+    profiling = zeros(ceil(constants.simulation_end / constants.dT_bac)+1, 10);
+    iProf = 1;
     %% time advancements (dT / dT_steadystate)
     
     while Time.current < constants.simulation_end
         % diffuse (MG)
+        tic;
         conc = diffusion(conc, reaction_matrix, bulk_concs, grid, constants);
-
+        profiling(iProf, 1) = profiling(iProf, 1) + toc;
+        
         % updata bacterial mass
+        tic;
         bac = update_bacterial_mass(bac, constants.dT);        
+        profiling(iProf, 2) = profiling(iProf, 2) + toc;
         
         % calculate reaction matrix
+        tic;
         [reaction_matrix, bac.mu, pH] = calculate_reaction_matrix(grid2bac, grid2nBacs, bac, grid, conc, constants, pH);
+        profiling(iProf, 3) = profiling(iProf, 3) + toc;
+
 
         % if T>T_ss: calculate residual
         if Time.current >= Time.steadystate
             
-            if steadystate_is_reached(conc, reaction_matrix, grid.dx, bulk_concs, constants)
+            tic;
+            ssReached = steadystate_is_reached(conc, reaction_matrix, grid.dx, bulk_concs, constants);
+            profiling(iProf, 4) = profiling(iProf, 4) + toc;
+    
+            if ssReached
                 % set time to next bacterial activity time
                 previousTime = Time.current;
                 Time.current = Time.bac;
@@ -61,7 +73,10 @@ function integTime(grid, bac, conc, directory, constants, init_params)
                     % reaction_matrix & mu & pH are already calculated (steady state, so still valid)
 
                     % update bacteria: mass
+                    tic;
                     bac = update_bacterial_mass(bac, dT_actual);
+                    profiling(iProf, 2) = profiling(iProf, 2) + toc;
+
 
                         %% time advancements (dT_divide)
                         if Time.current >= Time.divide
@@ -69,14 +84,19 @@ function integTime(grid, bac, conc, directory, constants, init_params)
                             Time.divide = Time.divide + constants.dT_divide;
                             
                             % determine radius bacteria from mass
+                            tic;
                             bac = update_bacterial_radius(bac, constants);
+                            profiling(iProf, 2) = profiling(iProf, 2) + toc;
+
                             
                             % bacteria: inactivate or die
+                            tic;
                             if constants.inactivationEnabled
                                 bac = bacteria_inactivate(bac, constants);
                             else
                                 bac = bacteria_die(bac, constants);
                             end
+                            
 
                             %{
                               <E: In the PREV model we are able to include some evolution-adaptation of bacteria (for example Ks, Ki, Yield etc) />
@@ -95,9 +115,12 @@ function integTime(grid, bac, conc, directory, constants, init_params)
                             
                             % bacteria: divide
                             bac = bacteria_divide(bac, constants);
+                            profiling(iProf, 5) = profiling(iProf, 5) + toc;
                             
                             % shove bacteria
-                            bac = bacteria_shove(bac, constants);
+                            tic;
+                            bac = bacteria_shove(bac, grid, constants);
+                            profiling(iProf, 6) = profiling(iProf, 6) + toc;
                             
                             %{
                               <E: Here we could add a constant to define if we want include detachment and how we simulate this detachment. />
@@ -105,21 +128,34 @@ function integTime(grid, bac, conc, directory, constants, init_params)
                               <C: agree, for now this works
                             %}
                             % bacteria: detachment
+                            tic;
                             bac = bacteria_detachment(bac, grid, constants);
+                            profiling(iProf, 7) = profiling(iProf, 7) + toc;
                             
                             % update/re-determine where bacs
+                            tic;
                             [grid2bac, grid2nBacs] = determine_where_bacteria_in_grid(grid, bac);
+                            profiling(iProf, 8) = profiling(iProf, 8) + toc;
                             
                             % update diffusion region
-                            diffusion_region = determine_diffusion_region(grid2bac, grid2nBacs, bac, grid, constants);
+                            tic;
+                            diffusion_region = determine_diffusion_region(grid2bac, grid2nBacs, bac, grid);
+                            profiling(iProf, 9) = profiling(iProf, 9) + toc;
                         end
                         
                     % calculate and set bulk concentrations
-                    bulk_concs = calculate_bulk_concentrations(constants, bulk_concs, invHRT, reactionMatrix, constants.dT);
+                    tic;
+                    [bulk_concs, invHRT] = calculate_bulk_concentrations(constants, bulk_concs, invHRT, reaction_matrix, constants.dT);
                     conc = set_concentrations(conc, bulk_concs, ~diffusion_region);
+                    profiling(iProf, 10) = profiling(iProf, 10) + toc;
                     
                     % recompute reaction matrix for next cycle
+                    tic;
                     [reaction_matrix, bac.mu, pH] = calculate_reaction_matrix(grid2bac, grid2nBacs, bac, grid, conc, constants, pH);
+                    profiling(iProf, 3) = profiling(iProf, 3) + toc;
+                    
+                    iProf = iProf + 1;
+                    fprintf('current simulation time: %.1f h', Time.current)
                 end
                 
                 % set next steadystate time
