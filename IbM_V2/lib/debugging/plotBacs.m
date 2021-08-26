7690a050ef4d113996a42fff9c7234ebbe5db2c3
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-08-26 22:39:53 +0200

    create debugging functions for visualisation

---
+++ IbM_V2/lib/debugging/plotBacs.m
@@ -0,0 +1,27 @@
+function plotBacs(g, bac)
+    % testing function to visualise bacteria
+    x = bac.x;
+    y = bac.y;
+    r = bac.radius;
+    s = bac.species;
+    
+    coloring = {'#D81B60', '#1E88E5', '#FFC107', '#004D40'};
+    
+    f = figure(2); clf;
+    f.Position = [-1800, 65, 1200, 900];
+    
+    for i = 1:size(x, 1)
+        rectangle('Curvature', [1 1], 'Position', [x(i) - r(i), y(i) - r(i), 2 * r(i), 2 * r(i)], 'LineWidth', 1, 'EdgeColor', [0, 0, 0], 'FaceColor', coloring{s(i)});
+    end
+    
+    axis equal;
+
+    xlim([0, g.nX*g.dx]);
+    ylim([0, g.nY*g.dy]);
+    xt = linspace(0, g.nX*g.dx, g.nX+1);
+    yt = linspace(0, g.nY*g.dy, g.nY+1);
+    xticks(xt);
+    yticks(yt);
+
+    grid on;
+end
