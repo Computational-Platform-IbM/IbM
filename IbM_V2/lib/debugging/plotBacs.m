1e0ebadd1bec73068945ba0a50303b5865f74108
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-09-02 16:37:45 +0200

    add debug visualisations

--- IbM_V2/lib/debugging/plotBacs.m
+++ IbM_V2/lib/debugging/plotBacs.m
@@ -5,10 +5,11 @@ function plotBacs(g, bac)
     r = bac.radius;
     s = bac.species;
     
-    coloring = {'#D81B60', '#1E88E5', '#FFC107', '#004D40'};
+    coloring = {'#D81B60', '#1E88E5', '#FFC107', '#004D40'}; % colorblind-friendly colours
     
     f = figure(2); clf;
-    f.Position = [-1800, 65, 1200, 900];
+%     f.Position = [-1800, 65, 1200, 900]; % desktop with two screens
+    f.Position = [60 60 1000 700];
     
     for i = 1:size(x, 1)
         rectangle('Curvature', [1 1], 'Position', [x(i) - r(i), y(i) - r(i), 2 * r(i), 2 * r(i)], 'LineWidth', 1, 'EdgeColor', [0, 0, 0], 'FaceColor', coloring{s(i)});
