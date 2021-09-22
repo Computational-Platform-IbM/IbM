e42bd51dc9ebf391007ff8a3db45cb849542191c
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-09-22 17:31:10 +0200

    fix bugs, improve speed, improve resilience

--- IbM_V2/lib/calculate_reaction_matrix.m
+++ IbM_V2/lib/calculate_reaction_matrix.m
@@ -1,4 +1,4 @@
-function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nBacs, bac, grid, conc, constants, pH)
+function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nBacs, bac, diffRegion, conc, constants, pH)
     % Calculate how much compounds are consumed/produced per grid cell due
     % to bacterial activity. Also updates the growth rate of the respective
     % bacteria.
@@ -34,9 +34,9 @@ function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nB
     mMetabolism = constants.MatrixMet;
     mDecay = constants.MatrixDecay;
     
-    iNH3 = strcmp(compounds, 'NH3');
-    iNO2 = strcmp(compounds, 'NO2');
-    iO2 = strcmp(compounds, 'O2');
+    iNH3 = find(strcmp(compounds, 'NH3'));
+    iNO2 = find(strcmp(compounds, 'NO2'));
+    iO2 = find(strcmp(compounds, 'O2'));
     
     reaction_matrix = zeros(size(conc));
     mu = zeros(size(bac.x));
@@ -44,7 +44,7 @@ function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nB
     % pre-compute for bulk-liquid (at 1,1 there should never be diffusion
     % layer)
     Sh_bulk = 10^-pH(1, 1);
-    [spcM, Sh_bulk] = solve_pH(Sh_bulk, [reshape(conc(1,1,:), [], 1, 1); 1; 0], Keq, chrM, constants.constantpH, constants.pHtolerance); % <C: why [...; 1; 0]? />
+    [spcM, Sh_bulk] = solve_pH(Sh_bulk, [reshape(conc(1,1,:), [], 1, 1); 1; 0], Keq, chrM, constants.constantpH, constants.pHtolerance); % <C: why [...; 1; 0]? /> => H2O & H+
     pH_bulk = -log10(Sh_bulk);
     
     % check if pH is solved correctly
@@ -54,15 +54,15 @@ function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nB
 
     
     % for each gridcell
-    for ix = 1:grid.nX % parfor?
-        for iy = 1:grid.nY
-            if ~grid2nBacs(ix, iy) % bulk layer
+    for ix = 1:size(conc, 1) % parfor?
+        for iy = 1:size(conc, 2)
+            if ~diffRegion(ix, iy) % bulk layer
                 pH(ix, iy) = pH_bulk;
                 
             else % in diffusion layer, thus pH calculation needs to be performed
                 % calculate pH & speciation
                 Sh_old = 10^-pH(ix, iy);
-                [spcM, Sh] = solve_pH(Sh_old, [reshape(conc(1,1,:), [], 1, 1); 1; 0], Keq, chrM, constants.constantpH, constants.pHtolerance); % <C: why [...; 1; 0]? />
+                [spcM, Sh] = solve_pH(Sh_old, [reshape(conc(ix,iy,:), [], 1, 1); 1; 0], Keq, chrM, constants.constantpH, constants.pHtolerance); % <C: why [...; 1; 0]? />
                 pH(ix, iy) = -log10(Sh);
                 
                 % check if pH is solved correctly
@@ -70,37 +70,38 @@ function [reaction_matrix, mu, pH] = calculate_reaction_matrix(grid2bac, grid2nB
                     warning('DEBUG:actionRequired', 'debug: negative concentration encountered after pH calculation...');
                 end
                 
-                
-                % get bacteria in this grid cell
-                iBacs = reshape(grid2bac(ix, iy, 1:grid2nBacs(ix, iy)), [], 1); % n-by-1 vector of bacterial indices
-                
-                speciesGrid = bac.species(iBacs);
-                unique_species = unique(speciesGrid); % which species
-                for i = 1:length(unique_species)
-                    curr_species = unique_species(i);
-                    
-                    % update mu_max per species based on pH
-                    [mu_max, maint] = determine_max_growth_rate_and_maint(curr_species, T, Sh);
+                if grid2nBacs(ix, iy)
+                    % get bacteria in this grid cell
+                    iBacs = reshape(grid2bac(ix, iy, 1:grid2nBacs(ix, iy)), [], 1); % n-by-1 vector of bacterial indices
 
-                    % get reactive concentrations for soluble components
-                    reactive_conc = [spcM(iNH3, reactive_form(iNH3)), ...
-                        spcM(iNO2, reactive_form(iNO2)), ...
-                        spcM(iO2, reactive_form(iO2))];
-                    
-                    % set mu for all bacteria of same species in that gridcell
-                    M = calculate_monod(Ks(curr_species,:), Ki(curr_species, :), reactive_conc);                        % [DN]
-                    mu_noMaintenance = mu_max * M;                                                                      % [1/h]
-                    mu_withMaintenance = mu_noMaintenance - maint;                                                      % [1/h]
-                    mu(iBacs(speciesGrid == curr_species)) = mu_withMaintenance;                                        % [1/h]
+                    speciesGrid = bac.species(iBacs);
+                    unique_species = unique(speciesGrid); % which species
+                    for i = 1:length(unique_species)
+                        curr_species = unique_species(i);
 
-                    % update reaction_matrix element for this grid cell
-                    concentrationChange = mMetabolism(:, curr_species) * mu_noMaintenance;                              % [molS/molX/h]
-                    if mu_withMaintenance < 0
-                        concentrationChange = concentrationChange - mDecay(:, curr_species) * mu_withMaintenance;       % [molS/molX/h]
+                        % update mu_max per species based on pH
+                        [mu_max, maint] = determine_max_growth_rate_and_maint(curr_species, T, Sh);
+
+                        % get reactive concentrations for soluble components
+                        reactive_conc = [spcM(iNH3, reactive_form(iNH3)), ...
+                            spcM(iNO2, reactive_form(iNO2)), ...
+                            spcM(iO2, reactive_form(iO2))];
+
+                        % set mu for all bacteria of same species in that gridcell
+                        M = calculate_monod(Ks(curr_species,:), Ki(curr_species, :), reactive_conc);                        % [DN]
+                        mu_noMaintenance = mu_max * M;                                                                      % [1/h]
+                        mu_withMaintenance = mu_noMaintenance - maint;                                                      % [1/h]
+                        mu(iBacs(speciesGrid == curr_species)) = mu_withMaintenance;                                        % [1/h]
+
+                        % update reaction_matrix element for this grid cell
+                        concentrationChange = mMetabolism(:, curr_species) * mu_noMaintenance;                              % [molS/molX/h]
+                        if mu_withMaintenance < 0
+                            concentrationChange = concentrationChange - mDecay(:, curr_species) * mu_withMaintenance;       % [molS/molX/h]
+                        end
+                        concentrationChange = concentrationChange * sum(bac.molarMass(iBacs(speciesGrid == curr_species))); % [molS/h]
+
+                        reaction_matrix(ix, iy, :) = reaction_matrix(ix, iy, :) + reshape(concentrationChange, 1,1,[]);     % [molS/h]
                     end
-                    concentrationChange = concentrationChange * sum(bac.molarMass(iBacs(speciesGrid == curr_species))); % [molS/h]
-                    
-                    reaction_matrix(ix, iy, :) = reaction_matrix(ix, iy, :) + reshape(concentrationChange, 1,1,[]);     % [molS/h]
                 end
             end
         end
@@ -172,7 +173,7 @@ function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
             %Error
             err = F / dF;
             % Newton-Raphson algorithm
-            Sh = Sh - err;
+            Sh = max(Sh - err, 1e-10);  %%% Set maximum pH value to 10, might need to be changed for different systems
 
             ipH = ipH + 1;
         end
@@ -181,6 +182,9 @@ function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
         end
         if (Sh < 1e-14) || (Sh > 1)
             warning('DEBUG:actionRequired', 'debug: pH found outside of 1-14 range...');
+            % this is not possible anymore, because of the 
+            % max(new_Sh, 1e-14) statement => 2x speed improvement w.r.t.
+            % bracketing solution
         end
     end
 end
