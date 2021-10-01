fabcb3d8194509147f96375b9a866a9c59e84302
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-10-01 11:03:37 +0200

    improve saving structure

--- IbM_V2/lib/post_processing/save_slice.m
+++ IbM_V2/lib/post_processing/save_slice.m
@@ -3,7 +3,10 @@ function save_slice(bac, conc, bulk_concentrations, pH, Time, grid, constants, d
     %
     % bac: struct containing all information regarding the bacteria
     % conc: matrix containing all concentrations per grid cell as of
-    %   (ix, iy, compound)    % pH:
+    %   (ix, iy, compound)
+    % bulk_concentrations: vector of the bulk liquid concentration of all
+    %   compounds
+    % pH: matrix containing the pH value per grid cell as (ix, iy)
     % Time: simulation time
     % grid: struct containing all information regarding the grid
     % constants: struct containing all simulation constants
@@ -13,9 +16,9 @@ function save_slice(bac, conc, bulk_concentrations, pH, Time, grid, constants, d
     
     results_file = [directory, '/results.mat'];
     if Time == 0
-        Results = init_save(constants, grid);
+        [bac_saved, conc_saved, pH_saved, reactor_saved] = init_save(constants, grid);
     else
-        Results = load(results_file);
+        [bac_saved, conc_saved, pH_saved, reactor_saved] = load(results_file);
     end
     
     %% set values
@@ -23,30 +26,30 @@ function save_slice(bac, conc, bulk_concentrations, pH, Time, grid, constants, d
     
     % bacterial variables
     nBacs = length(bac.x);
-    Results.bac.x(iSave, 1:nBacs) = bac.x;
-    Results.bac.y(iSave, 1:nBacs) = bac.y;
-    Results.bac.radius(iSave, 1:nBacs) = bac.radius;
-    Results.bac.species(iSave, 1:nBacs) = bac.species;
-    Results.bac.active(iSave, 1:nBacs) = bac.active;
+    bac_saved.x(iSave, 1:nBacs) = bac.x;
+    bac_saved.y(iSave, 1:nBacs) = bac.y;
+    bac_saved.radius(iSave, 1:nBacs) = bac.radius;
+    bac_saved.species(iSave, 1:nBacs) = bac.species;
+    bac_saved.active(iSave, 1:nBacs) = bac.active;
     
     % concentration variable
-    Results.conc(iSave, :) = conc;
+    conc_saved(iSave, :) = conc;
     
     % pH variable
-    Results.pH(iSave, :) = pH;
+    pH_saved(iSave, :) = pH;
     
     % reactor properties
-    Results.reactor.bulk_concs(iSave, :) = bulk_concentrations;
-    Results.reactor.HRT(iSave) = 1 / invHRT;
-    Results.reactor.granule_density(iSave) = sum(bac.molarMass * constants.bac_MW) / ...
+    reactor_saved.bulk_concs(iSave, :) = bulk_concentrations;
+    reactor_saved.HRT(iSave) = 1 / invHRT;
+    reactor_saved.granule_density(iSave) = sum(bac.molarMass * constants.bac_MW) / ...
         ( (max(bac.y) - min(bac.y)) * (max(bac.x) - min(bac.x)) * 1e-6 );
     
     %% save struct to file
-    save(results_file, 'Results', '-v7.3'); % <C: -v7.3 is default? v7.3 file is more than twice the size of v7 file... but still only 100 kB />
+    save(results_file, 'bac_saved', 'conc_saved', 'pH_saved', 'reactor_saved', '-v7.3'); % <C: -v7.3 is default? v7.3 file is more than twice the size of v7 file... but still only 100 kB />
     
 end
 
-function Results = init_save(constants, grid)
+function [bac_saved, conc_saved, pH_saved, reactor_saved] = init_save(constants, grid)
     % initialise the results struct for the number of saves that are going
     % to happen. Utilizes datatypes within the required precision with the
     % lowest storage requirements.
@@ -59,23 +62,22 @@ function Results = init_save(constants, grid)
     
     nSaves = ceil(constants.simulation_end / constants.dT_save) + 1;
 
-    Results = struct;
-    
     % bacterial variables
-    Results.bac.x = zeros(nSaves, constants.max_nBac, 'single');
-    Results.bac.y = zeros(nSaves, constants.max_nBac, 'single');
-    Results.bac.radius = zeros(nSaves, constants.max_nBac, 'single');
-    Results.bac.species = zeros(nSaves, constants.max_nBac, 'int8');
-    Results.bac.active = zeros(nSaves, constants.max_nBac, 'logical');
+    bac_saved = struct;
+    bac_saved.x = zeros(nSaves, constants.max_nBac, 'single');
+    bac_saved.y = zeros(nSaves, constants.max_nBac, 'single');
+    bac_saved.radius = zeros(nSaves, constants.max_nBac, 'single');
+    bac_saved.species = zeros(nSaves, constants.max_nBac, 'uint8');
+    bac_saved.active = zeros(nSaves, constants.max_nBac, 'logical');
     
     % concentration variable
-    Results.conc = zeros(nSaves, grid.nX, 'single');
+    conc_saved = zeros(nSaves, grid.nX, 'single');
     
     % pH variable
-    Results.pH = zeros(nSaves, grid.nX, 'single');
+    pH_saved = zeros(nSaves, grid.nX, 'single');
     
     % reactor properties
-    Results.reactor.bulk_concs = zeros(nSaves, sum(constants.isLiquid), 'single');
-    Results.reactor.HRT = zeros(nSaves, 1, 'single');
-    Results.reactor.granule_density = zeros(nSaves, 1, 'single');
+    reactor_saved.bulk_concs = zeros(nSaves, sum(constants.isLiquid), 'single');
+    reactor_saved.HRT = zeros(nSaves, 1, 'single');
+    reactor_saved.granule_density = zeros(nSaves, 1, 'single');
 end
