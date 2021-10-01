c51438274187fef751aadeefe9feb3c9842d3812
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-10-01 16:04:45 +0200

    apply different loading .mat file

--- IbM_V2/testingParallelRMatrix.m
+++ IbM_V2/testingParallelRMatrix.m
@@ -5,15 +5,16 @@ Main Takeaway: overhead of parfor is too large for efficient
 parallelisation... (for a 32x32 system with 500 bacteria), at a certain
 point this does become better with more bacteria (and more chunks);
 %}
-% load('testingRMatrix.mat') 
-load('testingRMatrix_large.mat')
-diffRegion = diffusion_region;
+% load('testingRMatrix.mat')
+load('testingRMatrix_medium.mat')
+% load('testingRMatrix_large.mat')
+% diffRegion = diffusion_region;
 
-grid.dx = 4e-6;
-grid.dy = grid.dx;
-grid.nX = size(diffRegion, 1); %32;
-grid.nY = size(diffRegion, 2); %32;
-grid.blayer_thickness = 5e-6;
+% grid.dx = 4e-6;
+% grid.dy = grid.dx;
+% grid.nX = grid.;%size(diffRegion, 1); %32;
+% grid.nY = 255;%size(diffRegion, 2); %32;
+% grid.blayer_thickness = 5e-6;
 
 
 %% determine diffRegion and focus region
@@ -256,80 +257,80 @@ end
 
 % end
 
-function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
-    % Solve the pH and speciation per grid cell
-    %
-    % <>
-    % original as copied from latest version, to be improved?
-    % </>
-    %
-    % -> spcM: species matrix for the respective pH
-    % -> Sh: proton concentration
-    % -> flagpH: flagpH = 1 -> Fix pH; flagpH = 0 -> Variable pH 
-    
-    w = 1;
-
-    if keepConstantpH
-        spcM = zeros(size(chrM));
-        Sh = Sh_ini;
-        Denm = (1 + Keq(:, 1) / w) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
-
-        spcM(:, 1) = ((Keq(:, 1) / w) .* StV * Sh^3) ./ Denm;
-        spcM(:, 2) = (StV * Sh^3) ./ Denm;
-        spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
-        spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
-        spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
-
-    else
-        % Newton-Raphson method
-        Sh = Sh_ini;
-        % Counter of convergences
-        ipH = 1; 
-        maxIter = 20;
-        err = 1; % initial error
-        F = 1; % initial electroneutrality error
-
-        % Inicialization of matrix of species
-        spcM = zeros(size(chrM)); dspcM = spcM;
-
-        while (abs(err) > Tol) && (abs(F) > Tol) && ipH <= maxIter
-            Denm = (1 + Keq(:, 1)) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
-            spcM(:, 1) = ((Keq(:, 1)) .* StV * Sh^3) ./ Denm;
-            spcM(:, 2) = (StV .* Sh^3) ./ Denm;
-            spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
-            spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
-            spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
-
-            % Evaluation of the charge balance for the current Sh value, F(Sh)
-            F = Sh + sum(spcM .* chrM, 'all');
-
-            % Calculation of all derivated functions
-            dDenm = Denm.^2;
-            aux = 3 * Sh^2 * (Keq(:, 1) + 1) + 2 * Sh * Keq(:, 2) + Keq(:, 2) .* Keq(:, 3);
-
-            dspcM(:, 1) = (3 * Sh^2 * Keq(:, 1) .* StV) ./ (Denm) - ((Keq(:, 1) .* StV * Sh^3) .* aux) ./ (dDenm);
-            dspcM(:, 2) = (3 * Sh^2 * StV) ./ Denm - (StV * Sh^3 .* aux) ./ dDenm;
-            dspcM(:, 3) = (2 * Sh * Keq(:, 2) .* StV) ./ Denm - ((Keq(:, 2) .* StV * Sh^2) .* aux) ./ dDenm;
-            dspcM(:, 4) = (Keq(:, 2) .* Keq(:, 3) .* StV) ./ Denm - ((Keq(:, 2) .* Keq(:, 3) .* StV * Sh) .* aux) ./ dDenm;
-            dspcM(:, 5) = -(Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4) .* StV .* aux) ./ dDenm;
-
-            % Evaluation of the charge balance for the current Sh value, dF(Sh)
-            dF = 1 + sum(dspcM .* chrM, 'all');
-            %Error
-            err = F / dF;
-            % Newton-Raphson algorithm
-            Sh = max(Sh - err, 1e-10);
-
-            ipH = ipH + 1;
-        end
-        if any(spcM < 0)
-            warning('DEBUG:actionRequired', 'debug: negative concentration encountered after pH calculation...');
-        end
-        if (Sh < 1e-14) || (Sh > 1)
-            warning('DEBUG:actionRequired', 'debug: pH found outside of 1-14 range...');
-        end
-    end
-end
+% function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
+%     % Solve the pH and speciation per grid cell
+%     %
+%     % <>
+%     % original as copied from latest version, to be improved?
+%     % </>
+%     %
+%     % -> spcM: species matrix for the respective pH
+%     % -> Sh: proton concentration
+%     % -> flagpH: flagpH = 1 -> Fix pH; flagpH = 0 -> Variable pH 
+%     
+%     w = 1;
+% 
+%     if keepConstantpH
+%         spcM = zeros(size(chrM));
+%         Sh = Sh_ini;
+%         Denm = (1 + Keq(:, 1) / w) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
+% 
+%         spcM(:, 1) = ((Keq(:, 1) / w) .* StV * Sh^3) ./ Denm;
+%         spcM(:, 2) = (StV * Sh^3) ./ Denm;
+%         spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
+%         spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
+%         spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
+% 
+%     else
+%         % Newton-Raphson method
+%         Sh = Sh_ini;
+%         % Counter of convergences
+%         ipH = 1; 
+%         maxIter = 20;
+%         err = 1; % initial error
+%         F = 1; % initial electroneutrality error
+% 
+%         % Inicialization of matrix of species
+%         spcM = zeros(size(chrM)); dspcM = spcM;
+% 
+%         while (abs(err) > Tol) && (abs(F) > Tol) && ipH <= maxIter
+%             Denm = (1 + Keq(:, 1)) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
+%             spcM(:, 1) = ((Keq(:, 1)) .* StV * Sh^3) ./ Denm;
+%             spcM(:, 2) = (StV .* Sh^3) ./ Denm;
+%             spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
+%             spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
+%             spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
+% 
+%             % Evaluation of the charge balance for the current Sh value, F(Sh)
+%             F = Sh + sum(spcM .* chrM, 'all');
+% 
+%             % Calculation of all derivated functions
+%             dDenm = Denm.^2;
+%             aux = 3 * Sh^2 * (Keq(:, 1) + 1) + 2 * Sh * Keq(:, 2) + Keq(:, 2) .* Keq(:, 3);
+% 
+%             dspcM(:, 1) = (3 * Sh^2 * Keq(:, 1) .* StV) ./ (Denm) - ((Keq(:, 1) .* StV * Sh^3) .* aux) ./ (dDenm);
+%             dspcM(:, 2) = (3 * Sh^2 * StV) ./ Denm - (StV * Sh^3 .* aux) ./ dDenm;
+%             dspcM(:, 3) = (2 * Sh * Keq(:, 2) .* StV) ./ Denm - ((Keq(:, 2) .* StV * Sh^2) .* aux) ./ dDenm;
+%             dspcM(:, 4) = (Keq(:, 2) .* Keq(:, 3) .* StV) ./ Denm - ((Keq(:, 2) .* Keq(:, 3) .* StV * Sh) .* aux) ./ dDenm;
+%             dspcM(:, 5) = -(Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4) .* StV .* aux) ./ dDenm;
+% 
+%             % Evaluation of the charge balance for the current Sh value, dF(Sh)
+%             dF = 1 + sum(dspcM .* chrM, 'all');
+%             %Error
+%             err = F / dF;
+%             % Newton-Raphson algorithm
+%             Sh = max(Sh - err, 1e-10);
+% 
+%             ipH = ipH + 1;
+%         end
+%         if any(spcM < 0)
+%             warning('DEBUG:actionRequired', 'debug: negative concentration encountered after pH calculation...');
+%         end
+%         if (Sh < 1e-14) || (Sh > 1)
+%             warning('DEBUG:actionRequired', 'debug: pH found outside of 1-14 range...');
+%         end
+%     end
+% end
 
 function [mu_max, maint] = determine_max_growth_rate_and_maint(species, T, Sh)
     % Determine the maximum growth rate and maintenance [h-1] for a
--- IbM_V2/testingParallelRMatrix2.m
+++ IbM_V2/testingParallelRMatrix2.m
@@ -7,14 +7,15 @@ point this does become better with more bacteria (and more chunks);
 %}
 
 % load('testingRMatrix.mat') % is not yet the focus region
-load('testingRMatrix_large.mat') % is already only the focus region
-diffRegion = diffusion_region;
+load('testingRMatrix_medium.mat')
+% load('testingRMatrix_large.mat') % is already only the focus region
+% diffRegion = diffusion_region;
 
-grid.dx = 4e-6;
-grid.dy = grid.dx;
-grid.nX = size(diffRegion, 1); %32;
-grid.nY = size(diffRegion, 2); %32;
-grid.blayer_thickness = 5e-6;
+% grid.dx = 4e-6;
+% grid.dy = grid.dx;
+% grid.nX = size(diffRegion, 1); %32;
+% grid.nY = size(diffRegion, 2); %32;
+% grid.blayer_thickness = 5e-6;
 
 %% determine diffRegion and focus region
 [diffRegion, focus_region] = determine_diffusion_region(grid2bac, grid2nBacs, bac, grid); 
@@ -123,7 +124,7 @@ tStart = tic; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     xRange = focus_region.x0:focus_region.x1;
     yRange = focus_region.y0:focus_region.y1;
     
-    for i = 1:100
+    for i = 1:10
         [reaction_matrix(xRange, yRange, :), mu, pH(xRange, yRange)] = ...
             rMatrix_chunk(pH(xRange, yRange), conc(xRange, yRange, :), grid2bac(xRange, yRange, :), grid2nBacs(xRange, yRange), diffRegion(xRange, yRange), ...
             grouped_bac, length(bac.x), 0, ...
@@ -217,80 +218,80 @@ end
 
 % end
 
-function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
-    % Solve the pH and speciation per grid cell
-    %
-    % <>
-    % original as copied from latest version, to be improved?
-    % </>
-    %
-    % -> spcM: species matrix for the respective pH
-    % -> Sh: proton concentration
-    % -> flagpH: flagpH = 1 -> Fix pH; flagpH = 0 -> Variable pH 
-    
-    w = 1;
-
-    if keepConstantpH
-        spcM = zeros(size(chrM));
-        Sh = Sh_ini;
-        Denm = (1 + Keq(:, 1) / w) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
-
-        spcM(:, 1) = ((Keq(:, 1) / w) .* StV * Sh^3) ./ Denm;
-        spcM(:, 2) = (StV * Sh^3) ./ Denm;
-        spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
-        spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
-        spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
-
-    else
-        % Newton-Raphson method
-        Sh = Sh_ini;
-        % Counter of convergences
-        ipH = 1; 
-        maxIter = 20;
-        err = 1; % initial error
-        F = 1; % initial electroneutrality error
-
-        % Inicialization of matrix of species
-        spcM = zeros(size(chrM)); dspcM = spcM;
-
-        while (abs(err) > Tol) && (abs(F) > Tol) && ipH <= maxIter
-            Denm = (1 + Keq(:, 1)) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
-            spcM(:, 1) = ((Keq(:, 1)) .* StV * Sh^3) ./ Denm;
-            spcM(:, 2) = (StV .* Sh^3) ./ Denm;
-            spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
-            spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
-            spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
-
-            % Evaluation of the charge balance for the current Sh value, F(Sh)
-            F = Sh + sum(spcM .* chrM, 'all');
-
-            % Calculation of all derivated functions
-            dDenm = Denm.^2;
-            aux = 3 * Sh^2 * (Keq(:, 1) + 1) + 2 * Sh * Keq(:, 2) + Keq(:, 2) .* Keq(:, 3);
-
-            dspcM(:, 1) = (3 * Sh^2 * Keq(:, 1) .* StV) ./ (Denm) - ((Keq(:, 1) .* StV * Sh^3) .* aux) ./ (dDenm);
-            dspcM(:, 2) = (3 * Sh^2 * StV) ./ Denm - (StV * Sh^3 .* aux) ./ dDenm;
-            dspcM(:, 3) = (2 * Sh * Keq(:, 2) .* StV) ./ Denm - ((Keq(:, 2) .* StV * Sh^2) .* aux) ./ dDenm;
-            dspcM(:, 4) = (Keq(:, 2) .* Keq(:, 3) .* StV) ./ Denm - ((Keq(:, 2) .* Keq(:, 3) .* StV * Sh) .* aux) ./ dDenm;
-            dspcM(:, 5) = -(Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4) .* StV .* aux) ./ dDenm;
-
-            % Evaluation of the charge balance for the current Sh value, dF(Sh)
-            dF = 1 + sum(dspcM .* chrM, 'all');
-            %Error
-            err = F / dF;
-            % Newton-Raphson algorithm
-            Sh = max(Sh - err, 1e-10);
-
-            ipH = ipH + 1;
-        end
-        if any(spcM < 0)
-            warning('DEBUG:actionRequired', 'debug: negative concentration encountered after pH calculation...');
-        end
-        if (Sh < 1e-14) || (Sh > 1)
-            warning('DEBUG:actionRequired', 'debug: pH found outside of 1-14 range...');
-        end
-    end
-end
+% function [spcM, Sh] = solve_pH(Sh_ini, StV, Keq, chrM, keepConstantpH, Tol)
+%     % Solve the pH and speciation per grid cell
+%     %
+%     % <>
+%     % original as copied from latest version, to be improved?
+%     % </>
+%     %
+%     % -> spcM: species matrix for the respective pH
+%     % -> Sh: proton concentration
+%     % -> flagpH: flagpH = 1 -> Fix pH; flagpH = 0 -> Variable pH 
+%     
+%     w = 1;
+% 
+%     if keepConstantpH
+%         spcM = zeros(size(chrM));
+%         Sh = Sh_ini;
+%         Denm = (1 + Keq(:, 1) / w) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
+% 
+%         spcM(:, 1) = ((Keq(:, 1) / w) .* StV * Sh^3) ./ Denm;
+%         spcM(:, 2) = (StV * Sh^3) ./ Denm;
+%         spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
+%         spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
+%         spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
+% 
+%     else
+%         % Newton-Raphson method
+%         Sh = Sh_ini;
+%         % Counter of convergences
+%         ipH = 1; 
+%         maxIter = 20;
+%         err = 1; % initial error
+%         F = 1; % initial electroneutrality error
+% 
+%         % Inicialization of matrix of species
+%         spcM = zeros(size(chrM)); dspcM = spcM;
+% 
+%         while (abs(err) > Tol) && (abs(F) > Tol) && ipH <= maxIter
+%             Denm = (1 + Keq(:, 1)) * Sh^3 + Keq(:, 2) * Sh^2 + Keq(:, 3) .* Keq(:, 2) * Sh + Keq(:, 4) .* Keq(:, 3) .* Keq(:, 2);
+%             spcM(:, 1) = ((Keq(:, 1)) .* StV * Sh^3) ./ Denm;
+%             spcM(:, 2) = (StV .* Sh^3) ./ Denm;
+%             spcM(:, 3) = (StV * Sh^2 .* Keq(:, 2)) ./ Denm;
+%             spcM(:, 4) = (StV * Sh .* Keq(:, 2) .* Keq(:, 3)) ./ Denm;
+%             spcM(:, 5) = (StV .* Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4)) ./ Denm;
+% 
+%             % Evaluation of the charge balance for the current Sh value, F(Sh)
+%             F = Sh + sum(spcM .* chrM, 'all');
+% 
+%             % Calculation of all derivated functions
+%             dDenm = Denm.^2;
+%             aux = 3 * Sh^2 * (Keq(:, 1) + 1) + 2 * Sh * Keq(:, 2) + Keq(:, 2) .* Keq(:, 3);
+% 
+%             dspcM(:, 1) = (3 * Sh^2 * Keq(:, 1) .* StV) ./ (Denm) - ((Keq(:, 1) .* StV * Sh^3) .* aux) ./ (dDenm);
+%             dspcM(:, 2) = (3 * Sh^2 * StV) ./ Denm - (StV * Sh^3 .* aux) ./ dDenm;
+%             dspcM(:, 3) = (2 * Sh * Keq(:, 2) .* StV) ./ Denm - ((Keq(:, 2) .* StV * Sh^2) .* aux) ./ dDenm;
+%             dspcM(:, 4) = (Keq(:, 2) .* Keq(:, 3) .* StV) ./ Denm - ((Keq(:, 2) .* Keq(:, 3) .* StV * Sh) .* aux) ./ dDenm;
+%             dspcM(:, 5) = -(Keq(:, 2) .* Keq(:, 3) .* Keq(:, 4) .* StV .* aux) ./ dDenm;
+% 
+%             % Evaluation of the charge balance for the current Sh value, dF(Sh)
+%             dF = 1 + sum(dspcM .* chrM, 'all');
+%             %Error
+%             err = F / dF;
+%             % Newton-Raphson algorithm
+%             Sh = max(Sh - err, 1e-10);
+% 
+%             ipH = ipH + 1;
+%         end
+%         if any(spcM < 0)
+%             warning('DEBUG:actionRequired', 'debug: negative concentration encountered after pH calculation...');
+%         end
+%         if (Sh < 1e-14) || (Sh > 1)
+%             warning('DEBUG:actionRequired', 'debug: pH found outside of 1-14 range...');
+%         end
+%     end
+% end
 
 function [mu_max, maint] = determine_max_growth_rate_and_maint(species, T, Sh)
     % Determine the maximum growth rate and maintenance [h-1] for a
