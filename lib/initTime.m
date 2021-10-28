1859e960467cf77d4379c2e34dea0cafef8e25bb
soundslikealloy <eloi.mrp@gmail.com>
2021-10-28 14:34:29 +0200

    "settings" as input in integTime and initTime

--- lib/initTime.m
+++ lib/initTime.m
@@ -1,4 +1,4 @@
-function [conc, bulk_concs, invHRT, reaction_matrix, pH, bac] = initTime(grid, bac, init_params, constants)
+function [conc, bulk_concs, invHRT, reaction_matrix, pH, bac] = initTime(grid, bac, init_params, constants, settings)
     %% initialisation
     % initiate values from preset parameters
     
@@ -30,6 +30,6 @@ function [conc, bulk_concs, invHRT, reaction_matrix, pH, bac] = initTime(grid, b
 
     % calculate reaction matrix
     [reaction_matrix(xRange, yRange, :), bac.mu, pH(xRange, yRange)] = calculate_reaction_matrix(grid2bac(xRange, yRange, :), ...
-        grid2nBacs(xRange, yRange), bac, diffusion_region(xRange, yRange, :), conc(xRange, yRange, :), constants, pH(xRange, yRange));
+        grid2nBacs(xRange, yRange), bac, diffusion_region(xRange, yRange, :), conc(xRange, yRange, :), constants, pH(xRange, yRange), settings);
     
 end
