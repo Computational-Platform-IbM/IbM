454f316fab2a5c6db5cb4a10bdb35c9113cd4c4b
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-11-10 23:27:10 +0100

    slight refactor

--- lib/dynamic_dT/dynamicDT_bac.m
+++ lib/dynamic_dT/dynamicDT_bac.m
@@ -8,22 +8,24 @@ function Time = dynamicDT_bac(Time, maxInitRES, iProf, constants)
     % Also sets the last time the dT_bac was changed, to prevent multiple
     % consecutive dT_bac changes.
     
-    initRES_above_threshold = maxInitRES(iProf) > constants.dynamicDT.initRESThresholdDecrease;
-    upward_trend = all(diff(maxInitRES((iProf - constants.dynamicDT.nIterThresholdIncrease + 1):iProf)) >= 0);
-    recently_changed = Time.current - Time.changed_dT_bac < constants.dynamicDT.nIterThresholdIncrease*Time.dT_bac;
-    if iProf >= constants.dynamicDT.nIterThresholdIncrease % can only check if iProf > n otherwise negative indexing
-        multiple_ss_with_low_initRES = all(maxInitRES(iProf - constants.dynamicDT.nIterThresholdIncrease + 1 : iProf ) < constants.dynamicDT.initRESThresholdIncrease);
-    else
-        multiple_ss_with_low_initRES = false;
-    end
+    
     
     
     if Time.current > 5
-        if  initRES_above_threshold && upward_trend 
-            Time.dT_bac = Time.dT_bac * 0.7;
-            Time.changed_dT_bac = Time.current;
-            fprintf(2, 'large initial RES value detected, dT_bac decreased to %g\n', Time.dT_bac);
-        elseif ~recently_changed && multiple_ss_with_low_initRES                
+%         initRES_above_threshold = maxInitRES(iProf) > constants.dynamicDT.initRESThresholdDecrease;
+%         upward_trend = all(diff(maxInitRES((iProf - constants.dynamicDT.nIterThresholdIncrease + 1):iProf)) >= 0);
+        recently_changed = Time.current - Time.changed_dT_bac < constants.dynamicDT.nIterThresholdIncrease*Time.dT_bac;
+        if iProf >= constants.dynamicDT.nIterThresholdIncrease % can only check if iProf > n otherwise negative indexing
+            multiple_ss_with_low_initRES = all(maxInitRES(iProf - constants.dynamicDT.nIterThresholdIncrease + 1 : iProf ) < constants.dynamicDT.initRESThresholdIncrease);
+        else
+            multiple_ss_with_low_initRES = false;
+        end
+%         if  initRES_above_threshold && upward_trend 
+%             Time.dT_bac = Time.dT_bac * 0.7;
+%             Time.changed_dT_bac = Time.current;
+%             fprintf(2, 'large initial RES value detected, dT_bac decreased to %g\n', Time.dT_bac);
+%         else
+        if ~recently_changed && multiple_ss_with_low_initRES
             Time.dT_bac = min(Time.dT_bac / 0.8, Time.maxDT_bac);
             Time.changed_dT_bac = Time.current;
             fprintf(2, 'multiple cycles with initRES value of <%.1f %%, dT_bac increased to %g\n', constants.dynamicDT.initRESThresholdIncrease*100, Time.dT_bac);                        
