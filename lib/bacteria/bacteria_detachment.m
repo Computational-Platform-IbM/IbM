8263aa298e7957307484e157e84e1ecd7f9a15d5
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-11-14 22:24:07 +0100

    remove bac.muRatio references

--- lib/bacteria/bacteria_detachment.m
+++ lib/bacteria/bacteria_detachment.m
@@ -18,6 +18,5 @@ function bac = bacteria_detachment(bac, grid, constants)
         bac.molarMass(bac_detach) = [];
         bac.active(bac_detach) = [];
         bac.mu(bac_detach) = [];
-        bac.muRatio(bac_detach) = [];
     end
 end
