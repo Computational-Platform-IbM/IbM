5349550ca5653a7f541c4be4434bc7429046d43b
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-11-17 14:08:35 +0100

    implement fast marching loop

--- lib/detachment/testDetachment.m
+++ lib/detachment/testDetachment.m
@@ -89,8 +89,38 @@ plotDetachTime(grid, T);
 
 
 
+% ------------ Fast Marching --------------
+for n = 1:10
+    % take narrow-band value with the lowest T value
+    [T_list, I] = sort(T(Narrow_band));
+    i = i(I);
+    j = j(I);
+
+    i_point = i(1);
+    j_point = j(1); 
+
+    % add point to visited
+    Narrow_band(i_point, j_point) = 0;
+    Visited(i_point, j_point) = 1;
+
+    % for all neighbours of the point (from narrow-band or far):
+    [Narrow_band, Far, T] = updateNeighbour(i_point+1, j_point, Narrow_band, Far, Visited, T, grid, kDet);
+    [Narrow_band, Far, T] = updateNeighbour(i_point-1, j_point, Narrow_band, Far, Visited, T, grid, kDet);
+
+    [Narrow_band, Far, T] = updateNeighbour(i_point, j_point+1, Narrow_band, Far, Visited, T, grid, kDet);
+    [Narrow_band, Far, T] = updateNeighbour(i_point, j_point-1, Narrow_band, Far, Visited, T, grid, kDet);
 
 
+
+
+
+
+
+    plotLogicalGrid(grid, Visited, 'Visited'); % {'Biofilm', 'Visited', 'Narrow band', 'Far'}
+    plotLogicalGrid(grid, Narrow_band, 'Narrow band'); % {'Biofilm', 'Visited', 'Narrow band', 'Far'}
+    plotLogicalGrid(grid, Far, 'Far'); % {'Biofilm', 'Visited', 'Narrow band', 'Far'}
+    plotDetachTime(grid, T);
+end
 
 
 
@@ -198,7 +228,121 @@ function Fdet = calculateLocalDetachmentRate(~, j, kDet, grid)
     Fdet = kDet * d^2;
 end
 
+function [Narrow_band, Far, T] = updateNeighbour(i, j, Narrow_band, Far, Visited, T, grid, kDet)
+    % update status of grid cell with regards to detachment and recalculate
+    % the Time-of-crossing using the quadratic approximation of the
+    % gradient.
+    
+    if Visited(i, j)
+        return
+    elseif Far(i, j) % remove from far and add to narrow band
+        Far(i, j) = 0;
+        Narrow_band(i, j) = 1;
+    end % if none of the above, then already in narrow band
+    
+    % recalculate T value
+    T(i, j) = recalculateT(T, i, j, kDet, grid);
+end
 
+function T_new = recalculateT(T, i, j, kDet, grid)
+    % Recalculate the T value at gridcell (i, j) using the quadratic 
+    % approximation of the gradient.
+    
+    Tx = zeros(3,1);
+    Tx(1) = min(T(i+1, j), T(i-1, j));
+    Tx(2) = max(T(i+1, j), T(i-1, j));
+    Tx(3) = Inf;
+    
+    Ty = zeros(3,1);
+    Ty(1) = min(T(i, j+1), T(i, j-1));
+    Ty(2) = max(T(i, j+1), T(i, j-1));
+    Ty(3) = Inf;
+    
+    if all(isinf(Tx)) && all(isinf(Ty))
+        error('all neighbours have infinite time of crossing')
+    end
+    
+    Fdet = calculateLocalDetachmentRate(i, j, kDet, grid);
+    
+    if Fdet == 0
+        error('Detachment speed equals 0, thus infinite time of crossing')
+    end
+    
+    approxSolution = 0;
+    validSolution = 0;
+    
+    for ix = 1:length(Tx)
+        Tx_neighbour = Tx(ix);
+        for iy = 1:length(Ty)
+            Ty_neighbour = Ty(iy);
+
+            temp = computeRoot(Tx_neighbour, Ty_neighbour, Fdet, grid.dx);
+            sols(ix, iy) = temp;
+            
+            if isfinite(temp)
+                approxSolution = max(temp, approxSolution);
+                if validSol(temp, Tx_neighbour, Tx(1)) || validSol(temp, Ty_neighbour, Ty(1))
+                    validSolution = max(temp, validSolution);
+                end
+            end
+        end
+    end
+    
+    if validSolution == 0
+        error('something went wrong with the root finding, pls check...')
+    end
+    
+    T_new = validSolution;
+    
+    
+    
+end
+
+function v = validSol(sol, f, t_neigh)
+    % check whether a given solution is valid.
+    
+    if isinf(f)
+        v = sol < t_neigh;
+    else
+        v = sol > t_neigh;
+    end        
+end
+
+
+function root = computeRoot(Tx, Ty, Fdet, dx)
+    % Solve the quadratic equation for the gradient (approximation)
+    
+    % first init all parameters
+    a = 0;
+    b = 0;
+    c = -(dx/Fdet)^2;
+    
+    % if Tx is finite, then add those respective terms to parameters
+    if isfinite(Tx)
+        a = a + 1;
+        b = b - 2*Tx;
+        c = c + Tx^2;
+    end
+    
+    % same for Ty
+    if isfinite(Ty)
+        a = a + 1;
+        b = b - 2*Ty;
+        c = c + Ty^2;
+    end
+    
+    % if all are infinite, then return inf
+    if a == 0
+        root = Inf;
+        return
+    end
+    
+    % now get the 2 solutions
+    D = sqrt(b^2 - 4*a*c);
+    
+    % positive solution is only valid
+    root = (-b+D) / (2*a);
+end
 
 %% plotting functions
 function pBac(bac, g)
@@ -248,39 +392,63 @@ end
 function plotLogicalGrid(grid, L, title_string)
     switch title_string
         case {'Biofilm', 'biofilm'}
-            figure(2);
+            f = figure(2);
         case {'Visited', 'visited'}
-            figure(3);
+            f = figure(3);
         case {'Narrow_band', 'narrow_band', 'Narrow band', 'narrow band'}
-            figure(4);
+            f = figure(4);
         case {'Far', 'far'}
-            figure(5);
+            f = figure(5);
         otherwise
             fprintf(2, 'no figure yet for %s, new figure created...\n', title_string);
-            figure();
+            f = figure();
     end
             
-    im = imagesc([grid.dx/2, grid.nX*grid.dx - grid.dx/2], [grid.dy/2, grid.nY*grid.dy - grid.dy/2], L'); hold on;
-%     im.AlphaData = 0.5;
-    colormap([0 0 0; 1 1 1]);
-    axis equal;
-    xlim([0, grid.nX*grid.dx]);
-    ylim([0, grid.nY*grid.dy]);
-    set(gca, 'YDir', 'normal'); 
-    title(title_string);
+    try
+        f.Children.Children(1).CData = L';
+        drawnow();
+    catch e
+        switch e.identifier
+            case 'MATLAB:noSuchMethodOrField' % has not been made before
+                im = imagesc([grid.dx/2, grid.nX*grid.dx - grid.dx/2], [grid.dy/2, grid.nY*grid.dy - grid.dy/2], L'); hold on;
+        %           im.AlphaData = 0.5;
+                colormap([0 0 0; 1 1 1]);
+                axis equal;
+                xlim([0, grid.nX*grid.dx]);
+                ylim([0, grid.nY*grid.dy]);
+                set(gca, 'YDir', 'normal'); 
+                title(title_string);
+            otherwise
+                rethrow(e)
+        end
+                
+    end
+    
 end
 
 function plotDetachTime(grid, T)
-    figure(6); clf;
-    imagesc([grid.dx/2, grid.nX*grid.dx - grid.dx/2], [grid.dy/2, grid.nY*grid.dy - grid.dy/2], T'); hold on;
-    colormap(viridis());
-    colorbar();
-    caxis([0, 1e-3])
-    axis equal;
-    xlim([0, grid.nX*grid.dx]);
-    ylim([0, grid.nY*grid.dy]);
-    set(gca, 'YDir', 'normal'); 
-    title('Detachment times [h]');
+    f = figure(6);
+    
+    try
+        ax = gca;
+        ax.Children(1).CData = T';
+        drawnow();
+    catch e
+        switch e.identifier
+            case 'MATLAB:noSuchMethodOrField' % has not been made before
+                imagesc([grid.dx/2, grid.nX*grid.dx - grid.dx/2], [grid.dy/2, grid.nY*grid.dy - grid.dy/2], T'); hold on;
+                colormap(viridis());
+                colorbar();
+                caxis([0, 1e-3])
+                axis equal;
+                xlim([0, grid.nX*grid.dx]);
+                ylim([0, grid.nY*grid.dy]);
+                set(gca, 'YDir', 'normal'); 
+                title('Detachment times [h]');
+            otherwise
+                rethrow(e)
+        end
+    end
 end
 
 
