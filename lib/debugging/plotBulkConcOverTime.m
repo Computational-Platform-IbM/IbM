3d3eff803e079fbe7e79983d519a5bd363ccd9ec
CvAmstel <73523938+CvAmstel@users.noreply.github.com>
2021-12-06 16:36:46 +0100

    failsafe if more than 8 compounds

--- lib/debugging/plotBulkConcOverTime.m
+++ lib/debugging/plotBulkConcOverTime.m
@@ -11,6 +11,9 @@ function f = plotBulkConcOverTime(bulk_history, time_history, constants)
     for subix = 1:nPlots(1)
         for subiy = 1:nPlots(2)
             plot_index = (subix - 1)*nPlots(2)+subiy;
+            if plot_index > length(constants.compoundNames)
+                return
+            end
             subplot(nPlots(1), nPlots(2), plot_index)
             plot(time_history, bulk_history(plot_index,:), 'LineWidth', 2);
             title(sprintf('Bulk concentration [%s] over time', constants.compoundNames{plot_index}));
