4c2fa74eb9c8bf81a2c4294b36c0adb9161ea9b1
soundslikealloy <eloi.mrp@gmail.com>
2022-06-17 09:15:31 +0100

    Stop reiterative dT minimum message

--- lib/dynamic_dT/decrease_dT_diffusion.m
+++ lib/dynamic_dT/decrease_dT_diffusion.m
@@ -7,7 +7,9 @@ function Time = decrease_dT_diffusion(Time, msg, dx, constants)
     if Time.dT ~= prev % only set changed dT time if actually changed
         Time.changed_dT = Time.current;
     end
-    fprintf(2, '%s, \n\tthus dT decreased to %g\n', msg, Time.dT)
-    fprintf(2, 'Neumann value of stability: %g\n', max(constants.diffusion_rates * Time.dT / (dx ^ 2)))
+    if Time.dT ~= constants.dynamicDT.minDT
+        fprintf(2, '%s, \n\tthus dT decreased to %g\n', msg, Time.dT)
+        fprintf(2, 'Neumann value of stability: %g\n', max(constants.diffusion_rates * Time.dT / (dx ^ 2)))
+    end
 end
 
